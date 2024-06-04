22d1d8c978a94a5884d77f1dc279b3dcb3d4c84a
Craig West <iwswordpress@gmail.com>
2024-06-04 11:58:09 +0100

    EXAMPLES

---
+++ 01_FOUNDATIONS/03_python_mock_examples/test_01_examples.py
@@ -0,0 +1,147 @@
+"""
+This test suite is intended to demonstrate sample usage of the python mock library.
+Much of this is already covered in the mock documentation, but this hopefully
+adds a few useful examples, calls out some subtleties, and addresses
+some common misunderstandings.
+
+It is intended to be read in-order, as a primer.
+Almost like a technical blog post written as a test suite.
+
+Run these tests with `python tests.py`
+"""
+
+from unittest import TestCase
+from unittest.mock import Mock, MagicMock
+
+
+################################################################################################
+# PART 1: The Mock class
+# The mock library has two major components that are separate, but work powerfully in tandem.
+# The first is the Mock class (and subclasses). The second are the 'patch' helpers.
+# First, we will demonstrate usage of the Mock class.
+################################################################################################
+
+
+class MockObjectBasics(TestCase):
+    """Demonstates the basics of the 'Mock' class."""
+
+    def test_mock(self):
+        """A Mock object is callable, and you can configure
+        this callable's return value. By default it returns
+        another Mock instance, created on first access.
+        Any arbitrary attribute access also creates & returns another
+        mock instance. This 'spreads' to form a tree of Mock objects"""
+
+        m = Mock()
+        self.assertIsInstance(m, Mock)
+
+        # a mock is always callable, and returns its return_value (by default, another Mock instance)
+        self.assertIsInstance(m(), Mock)
+
+        # any arbitrary attribute access or invocation will return another mock
+        self.assertIsInstance(m.xyz, Mock)
+        self.assertIsInstance(m.foo(), Mock)
+        self.assertIsInstance(m.just.keep.on().trucking, Mock)
+
+        # different attributes will all have different mock objects returned
+        self.assertNotEqual(m.abcd, m.efgh)
+
+        # but once referenced, the same mock is always returned
+        self.assertEqual(m.abcd, m.abcd)
+
+    def test_return_value(self):
+        """The Mock constructor takes some optional arguments to control the Mock's behavior.
+        return_value allows you to control what is returned when the Mock is called like a function.
+        You can change it at after construction as well."""
+        m = MagicMock(return_value=123)
+        self.assertEqual(123, m())
+        m.return_value = 345
+        self.assertEqual(345, m())
+
+    def test_mock_kwargs_constructor(self):
+        """In addition to the 6 supported arguments to the Mock constructor ("return_value", "side_effect", "spec", "spec_set", "wraps", and "name"),
+        arbitrary keyword arguments can be passed in, and these will be used to set attributes on child mocks after they are created.
+        One can even set attributes on child mock elements arbitrarily deep"""
+        m = Mock(
+            **{
+                "first_name": "owned",
+                "calculate_minimum.return_value": 456,
+                "company.xyz.get_url.side_effect": Exception,
+            }
+        )
+        self.assertEqual("owned", m.first_name)
+        self.assertEqual(456, m.calculate_minimum())
+        self.assertRaises(Exception, m.company.xyz.get_url)
+
+    def test_mock_magic_methods(self):
+        """The Mock class supports replacing Python magic methods.
+        The MagicMock class (covered later) supplies useful default implementations for most of them.
+        """
+
+        def my_str(self):
+            return "owned"
+
+        m = Mock()
+        m.__str__ = my_str
+        self.assertEqual("owned", str(m))
+
+    def test_mock_called(self):
+        """You can use the special "called" attribute to check that a Mock object was called"""
+        m = Mock()
+        self.assertFalse(m.called)
+        m()
+        self.assertTrue(m.called)
+
+
+class MockObjectSideEffects(TestCase):
+    """Another optional argument to the Mock constructor is side_effect. It has several different use cases.
+    It can be used for dynamic return values, for making other things happen whenever
+    a mock is called, or for raising exceptions. If you give it an iterable, it will iterate over the elements in
+    that iterable as successive return values."""
+
+    def test_side_effect_with_iterable(self):
+        """Here is an example of giving an iterable as side_effect"""
+
+        m = Mock(side_effect=range(4))
+        self.assertEqual(0, m())
+        self.assertEqual(1, m())
+        self.assertEqual(2, m())
+        self.assertEqual(3, m())
+
+        # will raise StopIteration when iterator is exhausted
+        self.assertRaises(StopIteration, m)
+
+    def test_side_effect_with_exception(self):
+        """You can give an Exception class or instance as a side_effect and that exception will be raised on every call"""
+
+        class MyException(Exception):
+            def __init__(self, detail):
+                self.detail = detail
+
+        # exception instance
+        m = Mock(side_effect=MyException("xyz"))
+        self.assertRaises(MyException, m)
+
+        # passing an exception class only works as expected if the exception has a no-arg constructor
+        m = Mock(side_effect=MyException)
+        self.assertRaises(
+            TypeError, m
+        )  # TypeError: __init__() takes exactly 2 arguments (1 given)
+
+        m = Mock(side_effect=KeyError)
+        self.assertRaises(KeyError, m)
+
+    def test_side_effect_with_callable(self):
+        """If you pass a callable as side_effect, it will be called with the same arguments
+        as the mock, and its return value will be used as the mock object's return value.
+        """
+
+        def my_side_effect(mock_arg):
+            if mock_arg % 2 == 0:
+                return "even"
+            else:
+                return "odd"
+
+        m = Mock(side_effect=my_side_effect)
+        self.assertEqual("even", m(2))
+        self.assertEqual("odd", m(3))
---
+++ 01_FOUNDATIONS/03_python_mock_examples/test_02_examples.py
@@ -0,0 +1,161 @@
+"""
+This test suite is intended to demonstrate sample usage of the python mock library.
+Much of this is already covered in the mock documentation, but this hopefully
+adds a few useful examples, calls out some subtleties, and addresses
+some common misunderstandings.
+
+It is intended to be read in-order, as a primer.
+Almost like a technical blog post written as a test suite.
+
+Run these tests with `python tests.py`
+"""
+
+import unittest
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+import mypackage.a
+import mypackage.b
+import mypackage.c
+
+
+################################################################################################
+# PART 2: The patch() helpers
+# The mock library has two major components that are separate, but work powerfully in tandem.
+# The first is the Mock class (and subclasses). The second are the 'patch' helpers.
+# Next, we will demonstrate how to use the patch helpers
+################################################################################################
+
+
+DEFAULT_VALUE = 1
+PATCHED_VALUE = 42
+QUERY_DATABASE_DEFAULT_VALUE = 1000000
+
+
+class PatchObjectExamples(TestCase):
+    """The purpose of patching is always to replace some ATTRIBUTE on some TARGET OBJECT.
+    That target object might be a user-defined object instance, a class object, or a module object.
+    The main patch() callable takes a string descriptor of this target object that is a source of confusion and
+    will be described later. It's easier to get started by describing the helper patch.object(), which
+    takes the target object as a direct parameter"""
+
+    def test_patch_object_as_func_decorator(self):
+        """There are several ways to invoke the patch helpers (including patch.object). One is as a function decorator.
+        my_instance._val is changed to PATCHED_VALUE when _test() is invoked, then set back to its old value afterwards
+        patch is literally going to call getattr(target, attr_name) to get the existing value & replace it by calling
+        setattr(target, attr_name, new) (later restoring it)
+        """
+        my_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+
+        @patch.object(my_instance, "_val", new=PATCHED_VALUE)
+        def _test():
+            self.assertEqual(PATCHED_VALUE, my_instance.foo())
+
+        _test()
+
+    def test_patch_object_as_context_processor(self):
+        """Patching can also be invoked as a context processor, using the "with" statement"""
+        my_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+        with patch.object(my_instance, "_val", new=PATCHED_VALUE):
+            self.assertEqual(PATCHED_VALUE, my_instance.foo())
+
+    def test_patch_object_as_class_decorator(self):
+        """As a special case, patching can be invoked as a class decorator. This is always for subclasses of
+        unittest.TestCase, and only methods prefixed with mock.path.TEST_PREFIX are patched ("test_" by default)
+        """
+
+        my_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+
+        @patch.object(my_instance, "_val", new=PATCHED_VALUE)
+        class MyNewTestClass(unittest.TestCase):
+
+            def test_one(self):
+                self.assertEqual(PATCHED_VALUE, my_instance.foo())
+
+            def test_two(self):
+                self.assertEqual(PATCHED_VALUE, my_instance.foo())
+
+            def other_method(self):
+                self.assertNotEqual(PATCHED_VALUE, my_instance.foo())
+                self.assertEqual(DEFAULT_VALUE, my_instance.foo())
+
+            def runTest(self):
+                # just need this due to declaration inside function
+                pass
+
+        test_class = MyNewTestClass()
+
+        # test_xxx methods have been patched
+        test_class.test_one()
+        test_class.test_two()
+
+        # this one was not
+        test_class.other_method()
+
+    def test_patch_object_on_instances(self):
+        """patch.object can patch individual class instances"""
+        my_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+        another_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+
+        with patch.object(my_instance, "foo", new=MagicMock(return_value=666)):
+            self.assertEqual(666, my_instance.foo())
+            self.assertEqual(DEFAULT_VALUE, another_instance.foo())
+
+        with patch.object(my_instance, "_val", new=777):
+            self.assertEqual(777, my_instance.foo())
+            self.assertEqual(DEFAULT_VALUE, another_instance.foo())
+
+    def test_patch_object_on_classes(self):
+        """patch.object can also patch a class object itself"""
+        my_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+        another_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+
+        with patch.object(mypackage.c.MyClass, "foo", new=MagicMock(return_value=666)):
+            self.assertEqual(666, my_instance.foo())
+            self.assertEqual(666, another_instance.foo())
+
+        @patch.object(mypackage.c.MyClass, "_val", new=777)
+        def test_patching_instance_var_on_class():
+            pass
+
+        # mypackage.c.MyClass does not have the attribute '_val', so this can't be done
+        self.assertRaises(AttributeError, test_patching_instance_var_on_class)
+
+    def test_patch_dict(self):
+        """patch.dict is a convenience for patching dict-like objects"""
+        d = {"a": 111, "b": 222}
+        with patch.dict(d, values={"a": 333}):
+            self.assertEqual(d["a"], 333)
+            self.assertEqual(d["b"], 222)
+
+
+class PatchTargetingExamples(TestCase):
+    """A lot of the confusion in patching comes when patching using the string targeting syntax."""
+
+    @patch("mypackage.a.A_MODULE_VAR", new=777)
+    def test_patch_module_variable(self):
+        """Patching module level variables works - keep in mind it will
+        only be in effect at patch time, so other dependent variables are unaffected"""
+        self.assertEqual(777, mypackage.a.A_MODULE_VAR)
+        self.assertEqual(777, mypackage.a.fn_referencing_module_var())
+        self.assertEqual(42 * 2, mypackage.a.A_DERIVED_MODULE_VAR)
+
+
+class PropertyExample(TestCase):
+    """Properties (Python descriptors in general) require some special effort to correctly patch"""
+
+    def test_patching_writable_property(self):
+        """If the property is writable, patching does work. need to patch the value, not the property definition function"""
+        my_instance = mypackage.c.MyClass(DEFAULT_VALUE)
+        self.assertEqual(my_instance.writeable_prop, DEFAULT_VALUE)
+
+        @patch.object(my_instance, "writeable_prop", new="xyz")
+        def test_patching_writable_property():
+            self.assertEqual("xyz", my_instance.writeable_prop)
+
+        # patching the writable property works
+        test_patching_writable_property()
+
+
+if __name__ == "__main__":
+    unittest.main()
