34a44fd6fccc6f0c46428d90da5c8cb3f0c68ab4
Craig West <iwswordpress@gmail.com>
2024-06-06 09:31:15 +0100

    add and reorg

--- 01_SECTION_FUNDAMENTALS/00_add_methods_dynamically/02_create_our_mock.py
+++ 01_SECTION_FUNDAMENTALS/00_add_methods_dynamically/02_create_our_mock.py
@@ -8,18 +8,18 @@ console = Console()
 
 
 # constructor
-def constructor(self, arg):
+def my_constructor(self, arg):
     self.constructor_arg = arg
 
 
 # method
-def displayMethod(self, arg):
+def my_displayMethod(self, arg):
     console.print(arg)
 
 
 # class method
 @classmethod
-def classMethod(cls, arg):
+def my_classMethod(cls, arg):
     console.print(arg)
 
 
@@ -29,14 +29,14 @@ Mock = type(
     (object,),  # inheritance of bases
     {
         # constructor
-        "__init__": constructor,
+        "__init__": my_constructor,
         # data members
         "string_attribute": "Our first mock",
         "int_attribute": 1706256,
         "mock_attribute": "a patched attribute",
         # member functions
-        "my_func_args": displayMethod,  # built in
-        "my_class_func": classMethod,
+        "my_func_args": my_displayMethod,  # built in
+        "my_class_func": my_classMethod,
         "my_lambda": lambda self, arg: lambda: console.print(arg),
         "lambda_square": lambda self, x: lambda: x * x,
     },
@@ -45,11 +45,12 @@ Mock = type(
 
 # creating objects
 obj = Mock("constructor argument")
+print("\n")
 console.print(obj.constructor_arg)
 console.print(obj.string_attribute)
 console.print(obj.int_attribute)
 print("\n======= obj.mock_attribute =======")
-console.print("\t\t", obj.mock_attribute)
+console.print("\t", obj.mock_attribute)
 print("======= obj.mock_attribute =======\n")
 obj.my_func_args("mock_arg")
 Mock.my_class_func("Class Dynamically Created !")
@@ -57,14 +58,14 @@ obj.my_lambda("hello")()
 result = obj.lambda_square(10)()
 console.print("result is ", result)
 
-console.print(globals())
+# console.print(globals())
 
-console.print(sys.modules["module01"])
+# console.print(sys.modules["module01"])
 
-sys.modules["module01"].say_hello()
+# sys.modules["module01"].say_hello()
 
-# we now patch module01 in to return PATCHED
+# # we now patch module01 in to return PATCHED
 
-sys.modules["module01"].say_hello = lambda: obj.mock_attribute
+# sys.modules["module01"].say_hello = lambda: obj.mock_attribute
 
-console.print("our patched say_hello()..", sys.modules["module01"].say_hello(), "\n\n")
+# console.print("our patched say_hello()..", sys.modules["module01"].say_hello(), "\n\n")
