10452cea227a599dcb66bbd14e99efed786e1f5b
Craig West <iwswordpress@gmail.com>
2024-06-10 13:55:12 +0100

    03_create

--- 01_SECTION_FUNDAMENTALS/00_add_methods_dynamically/03_create_test_double.py
+++ 01_SECTION_FUNDAMENTALS/00_add_methods_dynamically/03_create_test_double.py
@@ -7,7 +7,7 @@ from rich.console import Console
 console = Console()
 
 
-class Mock(object):
+class TestDouble(object):
 
     def __init__(self, msg):
         self.constructor_arg = msg
@@ -18,7 +18,7 @@ class Mock(object):
 
 # creating objects
 
-obj = Mock("PATCHED")
+obj = TestDouble("[red bold]TestDouble VALUE[/red bold]")
 
 print("\n")
 
@@ -33,21 +33,22 @@ original_say_hello = sys.modules["module02"].say_hello(
 # console.print(sys.modules["module02"])
 
 
-def return_mock_attribute():
+def return_test_double():
     return "[yellow bold]MOCKED[/yellow bold]"
 
 
-obj.mocked_say_hello = return_mock_attribute
 # patch say_hello() to use return_mock_attribute
 sys.modules["module02"].say_hello = obj.mocked_say_hello
 # sys.modules["module01"].say_hello = lambda: obj.mock_attribute
 # original say_hello()
-console.print("Patched ID", original_id)
+console.print("Original ID", original_id)
 console.print("our original say_hello()..", original_say_hello)
 
 # patched say_hello()
-console.print("Patched ID", str(id(sys.modules["module02"].say_hello))[-6:-1])
-console.print("our patched say_hello()..", sys.modules["module02"].say_hello(), "\n\n")
+console.print("TestDouble ID", str(id(sys.modules["module02"].say_hello))[-6:-1])
+console.print(
+    "our TestDouble say_hello()..", sys.modules["module02"].say_hello(), "\n\n"
+)
 
 
 # console.print(globals())
